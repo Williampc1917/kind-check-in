715d74440d98aa36d72dacd4d130e320a8967bf1
William <williampc1917@gmail.com>
2026-03-31 11:06:21 -0400

    fix basename

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/kind-check-in">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/privacy" element={<Privacy />} />
